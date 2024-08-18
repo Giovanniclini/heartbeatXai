a0efc70ee37f4143b1fe09de3efd153f4657f0c1
Giovanni Clini <gioviclini@gmail.com>
2024-08-18 21:01:25 +0200

    updated denoising and normalizing class

--- utils/denoising.py
+++ utils/denoising.py
@@ -1,4 +1,6 @@
 from scipy.signal import medfilt
+from scipy.signal import savgol_filter
+from sklearn.preprocessing import MinMaxScaler
 import dtcwt
 import numpy as np
 
@@ -23,8 +25,8 @@ def ensure_odd(number):
     return number if number % 2 != 0 else number + 1
 
 def remove_baseline_wander(signal, fs):
-    width_p_qrs = ensure_odd(int(0.2 * fs))  # 200 ms width for P wave and QRS complex
-    width_t_wave = ensure_odd(int(0.6 * fs))  # 600 ms width for T waves
+    width_p_qrs = ensure_odd(min(len(signal), int(0.2 * fs)))  # 200 ms width for P wave and QRS complex
+    width_t_wave = ensure_odd(min(len(signal), int(0.6 * fs)))  # 600 ms width for T waves
 
     baseline_p_qrs = medfilt(signal, kernel_size=width_p_qrs)
     baseline_t_wave = medfilt(signal, kernel_size=width_t_wave)
@@ -35,9 +37,20 @@ def remove_baseline_wander(signal, fs):
     signal_denoised = signal - baseline
     return signal_denoised
 
-def denoise_ecg(signal, fs):
+def smooth_signal(signal, window_length=5, polyorder=2):
+    return savgol_filter(signal, window_length=window_length, polyorder=polyorder)
+
+def normalise_and_denoise_ecg(signal, fs):
+    # Apply DTCWT denoising
     denoised_signal = dtcwt_denoise(signal)
 
+    # Remove baseline wander
     final_signal = remove_baseline_wander(denoised_signal, fs)
+
+    # Apply MinMax scaling
+    scaler = MinMaxScaler(feature_range=(-1, 1))
+    final_signal = scaler.fit_transform(final_signal.reshape(-1, 1)).flatten()
+
+    final_signal = smooth_signal(final_signal)
 
     return final_signal
